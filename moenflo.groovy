8074d1fd900dfde5929736f7fbd043a47ea621bf
David Manuel <dmanuel@fionta.com>
2020-07-12 17:42:58 -0700

    version and readme updates

--- moenflo.groovy
+++ moenflo.groovy
@@ -4,7 +4,7 @@
  * ANY KIND, either express or implied. See the License for the specific language governing permissions and 
  * limitations under the License.
  *
- * 2020-07-12 v0.1 - Initial Release
+ * 2020-07-12 v0.1-alpha - Initial Release
  * 
  */
 
